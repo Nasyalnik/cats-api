563e72ff3833da30eaa1820f413321a93a4d174f
a.a.sayadov <a.a.sayadov@tinkoff.ru>
2020-02-05 15:05:35 +0700

    * fix test

--- cats-api-photo/__tests__/api-test.js
+++ cats-api-photo/__tests__/api-test.js
@@ -2,7 +2,18 @@ const supertest = require('supertest');
 const app = require('../src/routes');
 const {pool}=require('../src/storage');
 
-describe('', () => {
+describe('Test urls', () => {
+  const catId = 5;
+
+  beforeAll(async (done) => {
+    await pool.query('TRUNCATE cats CASCADE');
+    await pool.query('TRUNCATE images');
+    await pool.query(
+        'INSERT INTO Cats(id, name, description, gender) VALUES ($1, $2, $3, $4)',
+        [catId, 'testCats', 'testCats', 'male'],
+      );
+    done();
+  })
   afterAll((done) => {
     pool.end();
     done();
@@ -10,7 +21,7 @@ describe('', () => {
 
   it('Add a cat image without image', async (done) => {
     const response = await supertest(app)
-        .post('/cats/1/upload');
+        .post(`/cats/${catId}/upload`);
     expect(response.status).toEqual(400);
     expect(response.body.output.statusCode).toEqual(400);
     expect(response.body.output.payload.statusCode).toEqual(400);
@@ -21,7 +32,7 @@ describe('', () => {
 
   it('Add a cat image with image', async (done) => {
     const response = await supertest(app)
-        .post('/cats/2/upload')
+        .post(`/cats/${catId}/upload`)
         .attach('file', `${__dirname}/test.png`);
     expect(response.status).toEqual(200);
     expect(response.body.fileUrl).toMatch(new RegExp('\/photos\/image-\\d+\.png'));
@@ -47,7 +58,7 @@ describe('', () => {
 
   it('Get image', async (done) => {
     const response = await supertest(app)
-        .get('/cats/2/photos');
+        .get(`/cats/${catId}/photos`);
     expect(response.status).toEqual(200);
     response.body.images.forEach((element) => {
       expect(element).toMatch(new RegExp('\/photos\/image-\\d+\.png'));
